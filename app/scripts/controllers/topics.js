9d14d9a659087d299baf2228af64e79cc6501fe3
Carl Vuorinen <carl.vuorinen@gmail.com>
2014-05-01 23:00:46 +0300

    List topics

--- app/scripts/controllers/topics.js
+++ app/scripts/controllers/topics.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('chatClientApp')
-  .controller('TopicsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('TopicsCtrl', function ($scope) {
+        $scope.topics = [
+            {id: 1, title: 'Symfony2 REST API'},
+            {id: 2, title: 'AngularJS'},
+            {id: 3, title: 'W3 School'}
+        ];
+    });
