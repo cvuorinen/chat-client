dd140210e30c2d02a77282c4e9f5f70ce9366978
Carl Vuorinen <carl.vuorinen@gmail.com>
2014-05-03 13:53:36 +0300

    Post message form

--- app/scripts/controllers/messages.js
+++ app/scripts/controllers/messages.js
@@ -2,5 +2,7 @@
 
 angular.module('chatClientApp')
     .controller('MessagesCtrl', function ($scope, Message) {
+        $scope.message = '';
+        $scope.user = {name: 'Carl'};
         $scope.messages = Message.query();
     });
