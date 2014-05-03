99e9626879b4561b19235f58eb19387cbb945c9b
Carl Vuorinen <carl.vuorinen@gmail.com>
2014-05-03 08:02:44 +0300

    Added descriptions to topics

--- app/scripts/services/topic.js
+++ app/scripts/services/topic.js
@@ -5,9 +5,9 @@ angular.module('chatClientApp')
         return {
             query: function() {
                 return [
-                    {id: 1, title: 'Symfony2 REST API'},
-                    {id: 2, title: 'AngularJS'},
-                    {id: 3, title: 'W3 School'}
+                    {id: 1, title: 'Symfony2 REST API', description: 'Talk about Symfony2 REST APIs and related topics, like FOS REST Bundle, JMS Serializer Bundle, Bazinga HATEOAS Bundle, Nelmio API Doc Bundle etc.'},
+                    {id: 2, title: 'AngularJS', description: 'Everything related to AngularJS'},
+                    {id: 3, title: 'W3 School', description: 'W3 School related discussion. Suggest new topics, give feedback and ask questions about previous topics.'}
                 ];
             }
         };
