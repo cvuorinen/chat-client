5f2c72387e61a69cb32b02c9467d04295f574083
Carl Vuorinen <carl.vuorinen@gmail.com>
2014-05-05 08:49:35 +0300

    Delete messages that were created by current user

--- app/scripts/controllers/messages.js
+++ app/scripts/controllers/messages.js
@@ -2,21 +2,47 @@
 
 angular.module('chatClientApp')
     .controller('MessagesCtrl', function ($scope, Message, SelectedTopic, User) {
-        $scope.message = new Message();
+        var createdMessages = [];
+
         $scope.user = User.getUser();
         $scope.topic = SelectedTopic.getSelectedTopic();
+        $scope.message = newMessage();
         $scope.messages = Message.query();
 
-        $scope.delete = Message.delete;
-
         $scope.save = function(message) {
-            $scope.message.user = $scope.user.name;
-            $scope.message._topic = {id: $scope.topic.id};
+            if (!$scope.message.id) {
+                createdMessages.push($scope.message);
+            }
 
             $scope.message.$save();
 
             $scope.messages.push($scope.message);
 
-            $scope.message = new Message();
+            $scope.message = newMessage();
+        }
+
+        $scope.delete = function(message) {
+            Message.delete(message);
+            _.remove($scope.messages, message);
+        }
+
+        $scope.createdMessage = function(message) {
+            var index = _.findIndex(createdMessages, {id: message.id});
+
+            return index >= 0;
+        }
+
+        function newMessage() {
+            var message = new Message();
+
+            if ($scope.user) {
+                message.user = $scope.user.name;
+            }
+
+            if ($scope.topic) {
+                message._topic = {id: $scope.topic.id};
+            }
+
+            return message;
         }
     });
