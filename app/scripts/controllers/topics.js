02552b5144f8ebaf65a3097eae2ad4df86ca9e44
Carl Vuorinen <carl.vuorinen@gmail.com>
2014-05-05 13:12:25 +0300

    Delete topic/message only after success response

--- app/scripts/controllers/topics.js
+++ app/scripts/controllers/topics.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('chatClientApp')
-    .controller('TopicsCtrl', function ($scope, $location, Topic, SelectedTopic) {
+    .controller('TopicsCtrl', function ($scope, $location, $window, Topic, SelectedTopic) {
         $scope.topics = Topic.query();
 
         $scope.delete = function(topic) {
-            Topic.delete(topic);
-            _.remove($scope.topics, topic);
+            Topic.delete(topic, function() {
+                _.remove($scope.topics, topic);
+            },
+            function() {
+                $window.alert('Delete failed. Cannot delete topic which has messages.');
+            });
         }
 
         $scope.selectTopic = function (topic) {
